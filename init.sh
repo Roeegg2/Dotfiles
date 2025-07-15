b3a42adcb93d0fd641d73be147c2403c3542698a
Roeegg2 <roeetoledano10@gmail.com>
2025-07-15 15:49:01 +0300

    Rework swaybar, some init updates and other improvements. From laptop

--- init.sh
+++ init.sh
@@ -10,8 +10,14 @@ ln -s ~/Dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/Dotfiles/sway ~/.config/sway
 ln -s ~/Dotfiles/nvim ~/.config/nvim
 ln -s ~/Dotfiles/foot ~/.config/foot
-ln -s ~/Dotfiles/proto /usr/share/fonts/proto
+ln -s ~/Dotfiles/waybar ~/.config/waybar
+ln -s ~/Dotfiles/blueman ~/.config/blueman
+sudo ln -s ~/Dotfiles/proto /usr/share/fonts/proto
 
-sudo pacman -S sway git neovim rust nodejs spotify-launcher foot lua clang gcc ripgrep fzf wget luarocks pavucontrol
+sudo pacman -S sway git neovim rust nodejs spotify-launcher foot lua clang gcc ripgrep fzf wget luarocks pavucontrol xorg-xwayland swaybg wmenu waybar networkmanager nm-connection-editor blueman pipewire pipewire-pulse pavucontrol wakatime network-manager-applet
+
+sudo systemctl enable --now NetworkManager.service
+sudo systemctl enable --now bluetooth.service
+systemctl --user enable --now pipewire.service pipewire-pulse.service
 
 mkdir -p ~/AUR
